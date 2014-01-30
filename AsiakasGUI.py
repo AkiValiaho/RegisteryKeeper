8cceb319c5a5ad870fd050e8e96ef9f1001de444
Aki Väliaho <akuli91@gmail.com>
2014-01-30 19:18:01 +0200

    Refreshed GUI package

--- AsiakasGUI.py
+++ AsiakasGUI.py
@@ -166,18 +166,4 @@ class AsiakasGUI:
             if choice == 2:
                 a = tkinter.simpledialog.askstring('Anna tallennettavan tiedoston nimi','Tiedoston nimi: ')
                 dumper.specificdump(self.nimilista,a)
-def main():
-    nimilista = {}
-    nimilista = dumper.load()
-    mainwindow = tkinter.Tk()
-    mainwindow.wm_title('Asiakashallintaohjelmisto')
-    menubar = tkinter.Menu(mainwindow)
 
-    #mainwindow.iconbitmap(default='transparent.ico') keksi uusi ikoni
-    asiakasGUI = AsiakasGUI(nimilista,mainwindow)
-    menubar.add_command(label='Lataa',command=lambda: asiakasGUI.newlistbox(1))
-    menubar.add_command(label='Tallenna',command=lambda: asiakasGUI.newlistbox(2))
-    mainwindow.protocol("WM_DELETE_WINDOW",asiakasGUI.eventhandler)
-    mainwindow.config(menu=menubar)
-    mainwindow.mainloop()
-main()
